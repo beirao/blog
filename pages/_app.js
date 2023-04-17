bdc13942c576493290e256386d4395ba2cf53ae7
THOMAS MARQUES <thomasmarques@THOMASs-MacBook-Pro.local>
2023-04-17 23:25:30 +0200

    vercel analytic added

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,14 @@ import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
 
+import { Analytics } from '@vercel/analytics/react'
 import '@fontsource/inter/variable-full.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
+// import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
