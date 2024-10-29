d009e19adfb0bb384308e7d961bace7116c2103f
Beirao <thomasmarques911@gmail.com>
2024-10-29 20:39:38 +0100

    Development portfolio added

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,8 @@
 const headerNavLinks = [
   { href: '/tags/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
-  { href: '/tags/reports', title: 'Audits' },
+  { href: '/tags/reports', title: 'Audit' },
+  { href: '/tags/engineering', title: 'Engineering' },
   { href: '/tools', title: 'Tools' },
   { href: '/portfolio', title: 'Portfolio' },
   { href: '/about', title: 'About' },
--- data/portfolioData.js
+++ data/portfolioData.js
@@ -1,27 +1,27 @@
 const portfolioData = [
   {
-    title: 'Code4rena',
+    title: 'Smart Contract Engineering',
+    description: `Showcase of my smart contract developments.`,
+    imgSrc: '/static/images/sc-engineering.webp',
+    href: '/tags/engineering',
+  },
+  {
+    title: 'Solo Review Reports',
+    description: `All my private audit reports.`,
+    imgSrc: '/static/images/solo-review.webp',
+    href: '/tags/reports',
+  },
+  {
+    title: 'Security Contests',
     description: `Showcase of my contest results.`,
     imgSrc: '/static/images/c4logo.jpeg',
     href: 'https://code4rena.com/@0xBeirao',
   },
   {
-    title: 'Solo Reviews Reports',
-    description: `All my private audit reports.`,
-    imgSrc: '/static/images/Solo_reviews.png',
-    href: '/tags/reports',
-  },
-  {
-    title: 'Bug Bounty',
-    description: `My bug bounty submissions.`,
+    title: 'Bug Hunting',
+    description: `Some of my bug bounty submissions.`,
     imgSrc: '/static/images/ImmuneFi-logo.webp',
     href: '/tags/bug-bounty',
-  },
-  {
-    title: 'Chainlink Spring 2023 Hackathon',
-    description: `Our submission for the Chainlink Spring 2023 Hackathon : Uniswap Max.`,
-    imgSrc: '/static/images/clh.png',
-    href: 'https://devpost.com/software/uniswap-max',
   },
 ]
 
